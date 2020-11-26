6a0f72c3f37866770ca7f4938ea4d38251c8630b
manasrajowar66 <58780138+manasrajowar66@users.noreply.github.com>
2020-11-26 22:54:23 +0530

    setup format of amount and created at

--- src/components/expenceListItem.js
+++ src/components/expenceListItem.js
@@ -3,10 +3,14 @@ import React from 'react';
 import { removeExpence } from '../actions/expences';
 import { Link } from 'react-router-dom';
 import EditExpensifyPage from './editExpensifyPage';
+import moment from 'moment';
+import numeral from 'numeral';
 const ExpenceListItem = ({describtion,amount,createdAt,id}) =>(
     <div>
         <Link className="link-decoration" to={`/edit/${id}`}><h2>{describtion}</h2></Link>
-        {amount} - {createdAt}
+        {numeral(amount/100).format('$0,0.00')}
+         - 
+         {moment(createdAt).format('MMMM Do, YYYY')}
     </div>
 );
 
