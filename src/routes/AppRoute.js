24e2fd969026b45751314e91fbe77e0ff6622403
manasrajowar66 <58780138+manasrajowar66@users.noreply.github.com>
2021-02-13 13:51:20 +0530

    init

--- src/routes/AppRoute.js
+++ src/routes/AppRoute.js
@@ -12,22 +12,17 @@ import PublicRoute from './PublicRoute';
 export const history = createBrowserHistory();
 
 const AppRoute = () => (
-
-         <Router history={history}>
-           <div>
-            <Switch>
-              <PublicRoute path="/" component={LoginPage} exact={true}/>
-              <PrivateRoute path="/dashboard" component={ExpensifyDashboardPage}/>
-              <PrivateRoute path="/create" component={CreateExpensifyPage} />
-              <PrivateRoute path="/edit/:id" component={EditExpensifyPage}/>
-              <Route component={NotFoundPage}/>
-           </Switch>
-
-        
-           </div>
-        
-        </Router>
-
+  <Router history={history}>
+    <div>
+      <Switch>
+        <PublicRoute path="/" component={LoginPage} exact={true} />
+        <PrivateRoute path="/dashboard" component={ExpensifyDashboardPage} />
+        <PrivateRoute path="/create" component={CreateExpensifyPage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensifyPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default AppRoute;
