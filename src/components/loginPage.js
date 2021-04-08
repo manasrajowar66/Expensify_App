c928e093719fa2f5b01e0221291724db7ff8c4bf
manasrajowar66 <58780138+manasrajowar66@users.noreply.github.com>
2021-04-08 17:26:16 +0530

    minor changes in text

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { startLogin } from '../actions/auth';
 import { connect } from 'react-redux';
 
-export const LoginPage = (props)=>(
-    <div className="box-layout">
-        <div className="box-layout__box">
-        <h1>Expensify</h1>
-        <p>It's the time to control your expences.</p>
-        <button className="button" onClick={props.startLogin}>Login with Google</button>
-        </div>
-        
+export const LoginPage = (props) => (
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1>Expensify</h1>
+      <p>It's the time to control your expenses.</p>
+      <button className="button" onClick={props.startLogin}>
+        Login with Google
+      </button>
     </div>
+  </div>
 );
 
 const mapDispatchToProps = (dispatch)=>{
